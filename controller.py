fa13c81bd8b119c3b606a050f5c2b0ed424b8c8d
Alexey <antonov1972@yandex.ru>
2023-04-10 14:13:51 +0300

    updated

--- controller.py
+++ controller.py
@@ -1,4 +1,5 @@
 
+from tkinter.scrolledtext import ScrolledText
 import functions
 import logger
 from tkinter import *
@@ -26,11 +27,19 @@ def mistake_entry(): # ошибочный ввод данных
         clear_entry()
         mistake_entry.mainloop()
 
+def mistake_entry_1(): # не заполнено поле Заглавие и текст
+        mistake_entry = Tk()
+        mistake_entry.title("Сообщение")
+        mistake_entry.geometry("300x30+1200+400")
+        label = Label(mistake_entry, text="Введите заглавие заметки и текст", font=("Arial", 12))
+        label.pack()
+        mistake_entry.mainloop()
+
 def mistake_entry_data(): # ошибочный ввод даты
         mistake_entry = Tk()
         mistake_entry.title("Сообщение")
         mistake_entry.geometry("300x90+1200+400")
-        label = Label(mistake_entry, text="На данную дату Заметок нет или Введена некорректная дата.\nВведите дату в формате 2023.03.15", font=("Arial", 11))
+        label = Label(mistake_entry, text="На данную дату Заметок нет \nили Введена некорректная дата.\nВведите дату в формате 2023.03.15", font=("Arial", 11))
         label.pack()
         clear_entry()
         mistake_entry.mainloop()
@@ -38,7 +47,7 @@ def mistake_entry_data(): # ошибочный ввод даты
 def enter1(event):# вывод в отдельном окне Списка абонентов
         window_text = Tk()
         window_text.title("Список заметок")
-        window_text.geometry("200x400+1400+400")
+        window_text.geometry("350x400+1400+400")
         t = Text(window_text)
         t.pack()
         t.insert(1.0, functions.func_1())
@@ -59,7 +68,7 @@ def enter3(event): # Удаление заметки
         functions.func_3()
         del_note = Tk() # вывод в отдельном окне удаленного абонента
         del_note.title("Заметка удалена")
-        del_note.geometry("200x200+1200+400")
+        del_note.geometry("400x200+1200+400")
         label_da = Label(del_note, text="", font=("Arial", 12), justify=LEFT)
         label_da["text"] = functions.func_3_1()
         label_da.pack()
@@ -75,7 +84,7 @@ def enter4_1(event): # редактирование абонента. кнопк
         functions.func_4_1()
         add_note = Tk() # вывод в отдельном окне добавленной заметки
         add_note.title("Заметка отредактирована")
-        add_note.geometry("200x200+1200+400")
+        add_note.geometry("300x200+1200+400")
         label_aa = Label(add_note, text="", font=("Arial", 12), justify=LEFT)
         label_aa["text"] = functions.func_4_2()
         label_aa.pack()
@@ -83,6 +92,7 @@ def enter4_1(event): # редактирование абонента. кнопк
         add_note.mainloop()
 
 def enter5(event): # Вывод в отдельном окне список заметок на указанную дату
+        functions.func_5()
         window_text = Tk()
         window_text.title("Список заметок")
         window_text.geometry("300x500+1400+400")
@@ -95,13 +105,20 @@ def enter5(event): # Вывод в отдельном окне список за
 def enter6(event):
         functions.func_6()
 
+def enter7(event): # во избежание нажатия клавиши Enter в поле Text (при нажатии Enter при редактировании текста заметки строка разбивается на список)
+        mistake_entry = Tk()
+        mistake_entry.title("Сообщение")
+        mistake_entry.geometry("300x90+1200+400")
+        label = Label(mistake_entry, text="Нажатие Enter неприменимо.\nДля сохранения изменений\nНажмите кнопку Сохранить изменения", font=("Arial", 12))
+        label.pack()
+        mistake_entry.mainloop()
+
 l2 = Label(root, text='Создать заметку. Заполните Заголовок и Текст заметки и нажмите "Добавить"', font=("Arial", 10)).place(x = 210, y = 10)
 l3 = Label(root, text='Удалить заметку. Введите Заголовок заметки и нажмите "Удалить"', font=("Arial", 10)).place(x = 210, y = 40)
 l4 = Label(root, text='Редактировать заметку. Введите Заголовок заметки и нажмите "Редактировать"', font=("Arial", 10)).place(x = 210, y = 70)
 l5 = Label(root, text='Показать Заметки на указанную дату.', font=("Arial", 10)).place(x = 210, y = 160)
 l5_2 = Label(root, text='Введите дату в формате гггг.мм.дд:', font=("Arial", 10)).place(x = 210, y = 190)
 l5_3 = Label(root, text=' и нажмите "Показать"', font=("Arial", 10)).place(x = 540, y = 190)
-
 
 b1 = Button(root, text="Просмотр всех заметок", width=25, height=1)
 b2 = Button(root, text="Добавить", width=13, height=1)
@@ -115,27 +132,21 @@ l_surname = Label(root, text='Заголовок', font=("Arial Black", 10)).pla
 l_name = Label(root, text='После редактирования заметки нажмите "Сохранить изменения"', font=("Arial", 10)).place(x = 210, y = 100)
 l_ph_number = Label(root, text='Текст заметки', font=("Arial Black", 10)).place(x = 10, y = 70)
 
-
 v1 = StringVar()
 v2 = StringVar()
 v3 = StringVar()
 v4 = StringVar() 
 v5 = StringVar()
 
-
 header = Entry(root, textvariable=v1, width=21, justify=LEFT,
         font=font.Font(family="Arial Black", size=10)) # поле для ввода Заголовка
 header.place(x=10, y=40)
-
 note_text = Text(root, width=26, height=10, 
-        font=font.Font(family="Arial", size=10)) # поле для ввода текста заметки
+        font=font.Font(family="Arial", size=10), wrap=WORD) # поле для ввода текста заметки
 note_text.place(x = 10, y = 100)
-
 time = Entry(root, textvariable=v4, width= 12, justify=LEFT,
         font=font.Font(family="Arial Black", size=10))
 time.place(x = 430, y = 190)
-
-
 
 b1.bind('<Button-1>', enter1)
 b1.place(x = 210, y = 230)
@@ -151,6 +162,8 @@ b5.bind('<Button-1>', enter5)
 b5.place(x = 700, y = 190)
 b6.bind('<Button-1>', enter6)
 b6.place(x = 210, y = 260)
+
+note_text.bind('<Return>', enter7) # нажатие клавиши Enter
 
 root.mainloop()
 logger.log("Программа закрыта")
--- data_base.py
+++ data_base.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-
 def list_view():
     d = Path('Notebook.txt')
     with open(d, 'r', encoding="utf-8") as data:
@@ -8,42 +7,29 @@ def list_view():
     
 
 def write_data_1(notes_list):
-    d = Path('Nb.txt')
+    d = Path('Notebook.txt')
     with open(d, 'w', encoding="utf-8") as data:
         for listitem in notes_list:
             data.writelines('%s\n' % listitem)
 
 def read_data_1():
     notes_list = []
-    d = Path('Nb.txt')
+    d = Path('Notebook.txt')
     with open(d, 'r', encoding="utf-8") as data:
         for line in data:
             currentPlace = line[:-1]
             notes_list.append(currentPlace)
         return notes_list
 
-def write_data(notes_list):
-    d = Path('Notebook.txt')
-    with open(d, 'w', encoding="utf-8") as data:
-        for i in range(0, int(len(notes_list))):
-            a = f"{notes_list[i]};\n"
-            data.writelines(a)
-
-def write_data1(id, header, text, data): # использовалась для редактирования
-    d = Path('Notebook.txt')
-    with open(d, 'a', encoding="utf-8") as data:
-        a = f"{id}{header}{text}{data}\n"
-        data.write(a)
-
 def write_read_csv():  # запись в csv файл
     import csv
     # получаем список строк из файла Notebook.txt
-    a = list(list_view().split())
+    a = read_data_1()
     z = len(a)
+    csv.register_dialect('my_di', delimiter=';')
     with open('Notebook.csv', 'w', newline='', encoding="utf-8") as data:
-        csvwriter = csv.writer(data)
-        # добавляем названия столбцов
-        csvwriter.writerow(['id'] + ['Заголовок'] + ['Заметка'] + ['Дата'])
+        csvwriter = csv.writer(data, 'my_di')
+        csvwriter.writerow(['id'] + ['Заголовок'] + ['Заметка'] + ['Дата'])# добавляем названия столбцов
         while z != 0:
             csvwriter.writerow([a[0], a[1], a[2], a[3]])  # построчная запись
             del a[:4]
@@ -51,4 +37,4 @@ def write_read_csv():  # запись в csv файл
     with open('Notebook.csv', 'r', newline='', encoding="utf-8") as data:  # читаем csv файл
         csvreader = csv.reader(data)
         for row in csvreader:
-            print(' '.join(row))  # и выводим на экран csv файл
+            print(''.join(row))  # и выводим на экран csv файл
